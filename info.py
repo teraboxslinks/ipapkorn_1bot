205a2ff5304a9ee6a12238d545dc17debec7b437
teraboxslinks <teraboxslinks@gmail.com>
2025-03-04 13:59:16 +0600

    Update info.py

--- info.py
+++ info.py
@@ -163,9 +163,9 @@ MULTI_CLIENT = True
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
-    ON_HEROKU = True
+    ON_HEROKU = False
 else:
-    ON_HEROKU = True
+    ON_HEROKU = False
 URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
 
